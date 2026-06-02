5ef41268fcb4a0db170a4a87f505728928cddb3e
auraecosystem <sandraisom@icloud.com>
2026-06-02 18:54:29 +0100

    Update Clone.sh

--- Clone.sh
+++ Clone.sh
@@ -1,8 +1,11 @@
+#cline
 forge init eip-7702-project
 cd eip-7702-project
 
 git clone https://github.com/quiknode-labs/qn-guide-examples.git
 cd qn-guide-examples/ethereum/eip-7702
-
+i
 forge install foundry-rs/forge-std
-forge install OpenZeppelin/openzeppelin-contracts
+forge install OpenZeppelin/openzeppelin-
+git clone --recursive https://github.com/argotorg/solidity.git
+cd solidity
